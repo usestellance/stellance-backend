af5a8c5a96cb773e6f7c784db9f9caf3e7428470
Davidhero <owogogahhero@outlook.com>
2025-06-21 12:57:54 +0100

    refactor: auth

--- internal/auth/dto.go
+++ internal/auth/dto.go
@@ -4,13 +4,14 @@ import "github.com/The-True-Hooha/stellance-backend.git/internal/user"
 
 type AuthRequestDto struct {
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8,containsany=!@#$%^&*"`
+	Password string `json:"password" validate:"required,passwd"`
 }
 
 type AuthResponseDto struct {
-	User               user.UserResponseDto `json:"user,omitempty"`
-	AccessToken        string               `json:"access_token,omitempty"`
-	RefreshToken       string               `json:"refresh_token,omitempty"`
-	ExpiresIn          int64                `json:"expires_in,omitempty"`
-	RefreshTokenExpiry int64                `json:"refresh_token_expiry,omitempty"`
+	User               user.UserProfileDto `json:"user,omitempty"`
+	AccessToken        string              `json:"access_token,omitempty"`
+	RefreshToken       string              `json:"refresh_token,omitempty"`
+	ExpiresIn          int64               `json:"expires_in,omitempty"`
+	RefreshTokenExpiry int64               `json:"refresh_token_expiry,omitempty"`
+	EmailToken         string              `json:"email_token,omitempty"`
 }
